ead535b766b7bd048c24395e53d9bd4b294f5105
Grzegorz Lang <rev3r@outlook.com>
2018-08-28 22:40:10 +0200

    Problem2

---
+++ Euler/Problems/Problem2.cs
@@ -0,0 +1,39 @@
+﻿namespace Euler.Problems
+{
+	internal class Problem2 : ProblemBase<int, int>
+	{
+		public Problem2() : base(4000000)
+		{
+		}
+
+		protected override int Solve(int input)
+		{
+			var sum = 2;
+
+			var last = 1;
+			var current = 2;
+			while (current <= input)
+			{
+				CalculateNextStep(ref last, ref current);
+				AddIfEven(ref sum, current);
+			}
+
+			return sum;
+		}
+
+		private void AddIfEven(ref int sum, int current)
+		{
+			if (current % 2 == 0)
+			{
+				sum += current;
+			}
+		}
+
+		private void CalculateNextStep(ref int last, ref int current)
+		{
+			var temp = last + current;
+			last = current;
+			current = temp;
+		}
+	}
+}
