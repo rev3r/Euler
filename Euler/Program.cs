bc12dfa9453b431c6dcced016bb96389d565f476
Grzegorz Lang <rev3r@outlook.com>
2018-08-29 00:54:59 +0200

    Include time

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -1,42 +1,38 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
-using System.Transactions;
 using static System.Console;
 
 namespace Euler
 {
 	internal class Program
 	{
+		private static (string solution, double time) GetSolution(int number)
+		{
+			var type = Assembly.GetExecutingAssembly()
+				.GetType("Euler.Problems.Problem" + number);
+			var instance = Activator.CreateInstance(type);
+			var prop = type.GetProperty("Solution");
+
+			var sw = Stopwatch.StartNew();
+			var result = prop.GetValue(instance, null);
+			sw.Stop();
+
+			return (result.ToString(),
+				sw.Elapsed.TotalMilliseconds);
+		}
+
 		private static void Main(string[] args)
 		{
-			Write("Problem to execute: ");
-
+			Write("Which problem to execute: ");
 			var choice = int.Parse(ReadLine());
-			var solution = GetSolution(choice);
 
-			WriteLine(solution);
-			WriteLine();
-		}
+			Clear();
+			WriteLine("Number:\t" + choice);
 
-		private static object GetSolution(int number)
-		{
-			var type = GetProblemClassType(number);
-
-			return GetPropertyValue(type, "Solution");
-		}
-
-		private static Type GetProblemClassType(int number)
-		{
-			return Assembly.GetExecutingAssembly()
-				.GetType("Euler.Problems.Problem" + number);
-		}
-
-		private static object GetPropertyValue(Type type, string propertyName)
-		{
-			var instance = Activator.CreateInstance(type);
-
-			return type.GetProperty(propertyName)
-				.GetValue(instance, null);
+			var (solution, time) = GetSolution(choice);
+			WriteLine("Result:\t" + solution);
+			WriteLine($"Time:\t{time}ms");
 		}
 	}
 }
